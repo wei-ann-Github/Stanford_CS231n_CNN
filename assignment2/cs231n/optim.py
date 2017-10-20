8e6073e2d7c6efd7a26c938966ed60678bdf6d7f
wei-ann-github <wei-ann.lim@outlook.com>
2017-10-20 10:40:03 +0800

    completed fullyconnectednets.ipynb except the last part on building a better classifier

--- assignment2/cs231n/optim.py
+++ assignment2/cs231n/optim.py
@@ -65,8 +65,9 @@ def sgd_momentum(w, dw, config=None):
     # TODO: Implement the momentum update formula. Store the updated value in #
     # the next_w variable. You should also use and update the velocity v.     #
     ###########################################################################
-    v = config['momentum'] * v + (1 - config['momentum']) * dw
-    next_w = w - config['learning_rate'] * v
+    v = config['momentum'] * v - config['learning_rate'] * dw # this equation is different from some other literatures where v = config['momentum'] * v + (1-config['momentum']) * dw. This stanford version does not keep momentum separate from what we traditionally know of how the parameters are changed by the learning rate.
+    # refer to http://cs231n.github.io/neural-networks-3/#sgd for Stanford's implementation.
+    next_w = w + v
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
@@ -100,7 +101,8 @@ def rmsprop(x, dx, config=None):
     # in the next_x variable. Don't forget to update cache value stored in    #
     # config['cache'].                                                        #
     ###########################################################################
-    pass
+    config['cache'] = config['decay_rate'] * config['cache'] + (1 - config['decay_rate']) * dx**2
+    next_x = x - config['learning_rate'] * dx / (np.sqrt(config['cache']) + config['epsilon'])
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
@@ -137,7 +139,12 @@ def adam(x, dx, config=None):
     # the next_x variable. Don't forget to update the m, v, and t variables   #
     # stored in config.                                                       #
     ###########################################################################
-    pass
+    config['t'] += 1
+    config['m'] = config['beta1'] * config['m'] + (1 - config['beta1']) * dx
+    m_corrected = config['m'] / (1 - config['beta1']**config['t'])
+    config['v'] = config['beta2'] * config['v'] + (1 - config['beta2']) * dx**2
+    v_corrected = config['v'] / (1 - config['beta2']**config['t'])
+    next_x = x - config['learning_rate'] * m_corrected / (np.sqrt(v_corrected) + config['epsilon'])
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
