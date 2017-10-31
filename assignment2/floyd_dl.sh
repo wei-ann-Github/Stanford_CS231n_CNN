f862059b25b75a47c905c5fbda5a1b87d1d1a8c5
wei-ann-github <wei-ann.lim@outlook.com>
2017-10-31 21:55:56 +0800

    added bash file to start project on floydehub

--- assignment2/floyd_dl.sh
+++ assignment2/floyd_dl.sh
@@ -2,7 +2,5 @@ echo "change directory to that for CS231 CNN for computer vision"
 cd d:
 cd "Wei Ann Lim\Documents\github\Stanford_CS231n_CNN\assignment2\floydhub"
 floyd login
-y
-eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2Zsb3lkaHViLmF1dGgwLmNvbS8iLCJzdWIiOiJhdXRoMHw1OWFmNTFlMWQyNWM4ZjVmN2M5OTJjZDQiLCJhdWQiOiJRZEl5NlJHM1Z5cVdyMWFmTnNhWVRGRTFDZlVXZnRVTSIsImlhdCI6MTUwOTI4MDU1MywiZXhwIjoxNTA5ODg1MzUzfQ.PWSFMCod3X1LbmkAPGd5Y3qHXGAwuHBaAmXZbr8WhCU
 floyd init cs231n
 floyd run --data wanormal/datasets/cs231n_cifar10/2:cs231n --gpu --env pytorch-0.2 --mode jupyter
